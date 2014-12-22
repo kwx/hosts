27182c54cb10ed6b6b41b03fd49cb7af230934d7
Ding Soung <DingSoung@gmail.com>
2014-12-22 13:22:33 +0800

    edit format

--- update.bat
+++ update.bat
@@ -1,4 +1,26 @@
 @echo off
 @ipconfig /flushdns
-@copy /y hosts C:\Windows\system32\drivers\etc\hosts
-@pause
+
+echo "basic hosts"
+copy /y hosts C:\Windows\system32\drivers\etc\hosts
+
+echo "google IP"
+while read Line
+do
+	echo $Line >> C:\Windows\system32\drivers\etc\hosts
+	echo $Line
+done < google
+
+echo "apple facebook twitter youtube dropbox and others ip"
+while read Line
+do
+echo $Line >> C:\Windows\system32\drivers\etc\hosts
+echo $Line
+done < others
+
+echo "block ads"
+while read Line
+do
+echo $Line >> C:\Windows\system32\drivers\etc\hosts
+echo $Line
+done < ads
