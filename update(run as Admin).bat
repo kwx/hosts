3b403d9bc69e37090fe32d12f1f0d778d1b80c01
Ding Soung <DingSoung@gmail.com>
2014-12-25 09:39:58 +0800

    update google ip ,windows update shell

--- update(run as Admin).bat
+++ update(run as Admin).bat
@@ -13,3 +13,6 @@
 
 @echo "block ads"
 @copy /b C:\Windows\system32\drivers\etc\hosts + ads C:\Windows\system32\drivers\etc\hosts
+
+@echo "如果总共显示拷贝了4个文件，表示更新成功，否则你需要确认是使用管理员权限，或手动获取hosts权限，或者检查360等安全位置软件问题"
+@pause
