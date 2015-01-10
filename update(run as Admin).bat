488a50b83851a839b6e2d747277cf87118a83181
Ding Soung <DingSoung@gmail.com>
2015-01-10 14:34:23 +0800

    cm

--- update(run as Admin).bat
+++ update(run as Admin).bat
@@ -1,17 +1,13 @@
 @echo off
-@ipconfig /flushdns
+ipconfig /flushdns
 
-@echo "basic hosts"
-@copy /y hosts C:\Windows\system32\drivers\etc\hosts	
+git pull
+cls
 
-@echo "google IP"
-@copy /b C:\Windows\system32\drivers\etc\hosts + google.txt  C:\Windows\system32\drivers\etc\hosts	
+copy /y hosts 	%windir%\System32\drivers\etc\hosts
+copy /b %windir%\System32\drivers\etc\hosts + google.txt  %windir%\System32\drivers\etc\hosts
+copy /b %windir%\System32\drivers\etc\hosts + others.txt  %windir%\System32\drivers\etc\hosts
+copy /b %windir%\System32\drivers\etc\hosts + ads.txt %windir%\System32\drivers\etc\hosts
 
-@echo "apple facebook twitter youtube dropbox and others ip"		
-@copy /b C:\Windows\system32\drivers\etc\hosts + others.txt  C:\Windows\system32\drivers\etc\hosts	
-
-@echo "block ads"
-@copy /b C:\Windows\system32\drivers\etc\hosts + ads.txt C:\Windows\system32\drivers\etc\hosts	
-
-@echo "it successed if you see 4 file copyed, else you need check if you run this bash as administrator, or killed by local security"
+echo "it successed if you see 4 file copyed, else you need check if you run this bash as administrator, or killed by local security"
 @pause
