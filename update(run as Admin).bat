485ad9b81c275a01b962670db7d79d7f57c24396
Ding Soung <DingSoung@gmail.com>
2015-01-24 15:48:27 +0800

    Update update(run as Admin).bat

--- update(run as Admin).bat
+++ update(run as Admin).bat
@@ -1,9 +1,11 @@
 @echo off
 cls
-copy /y hosts.txt 	%windir%\System32\drivers\etc\hosts
-copy /b %windir%\System32\drivers\etc\hosts + google.txt  %windir%\System32\drivers\etc\hosts
-copy /b %windir%\System32\drivers\etc\hosts + others.txt  %windir%\System32\drivers\etc\hosts
-copy /b %windir%\System32\drivers\etc\hosts + ads.txt %windir%\System32\drivers\etc\hosts
+copy /y	hosts.txt 	hosts
+copy /b	hosts	+	google.txt	hosts
+copy /b	hosts	+	others.txt	hosts
+copy /b	hosts	+	ads.txt		hosts
+copy /y	hosts	%windir%\System32\drivers\etc\hosts
 ipconfig /flushdns
-echo "it successed if you see 4 file copyed, else you need check if you run this bash as administrator, or killed by local security"
+echo "if you see the laste file copy failed,you may need check if you run this bash as administrator,or move the hosts to %windir%\System32\drivers\etc\ handly"
+
 @pause
