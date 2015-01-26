aee610013f35d96b2b3bd6add8a63805028f9512
Ding Soung <DingSoung@gmail.com>
2015-01-26 10:11:46 +0800

    improve speed

--- update(run as Admin).bat
+++ update(run as Admin).bat
@@ -4,8 +4,10 @@ copy /y	hosts.txt 	hosts
 copy /b	hosts	+	google.txt	hosts
 copy /b	hosts	+	others.txt	hosts
 copy /b	hosts	+	ads.txt		hosts
+cls
 copy /y	hosts	%windir%\System32\drivers\etc\hosts
 ipconfig /flushdns
-echo "if you see the laste file copy failed,you may need check if you run this bash as administrator,or move the hosts to %windir%\System32\drivers\etc\ handly"
+
+echo "if you see the l file copy failed,you may need check if you run this bash as administrator,or move the hosts to %windir%\System32\drivers\etc\ handly"
 
 @pause
