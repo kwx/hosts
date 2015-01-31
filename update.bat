cf24755fc368a406653b5decfc8d4d3df9ef16d8
Ding Soung <DingSoung@gmail.com>
2015-01-31 17:21:49 +0800

    Update and rename update(run as Admin).bat to update.bat

--- update.bat
+++ update.bat
@@ -7,7 +7,5 @@ copy /b	hosts	+	ads.txt		hosts
 cls
 copy /y	hosts	%windir%\System32\drivers\etc\hosts
 ipconfig /flushdns
-
 echo "if you see the l file copy failed,you may need check if you run this bash as administrator,or move the hosts to %windir%\System32\drivers\etc\ handly"
-
 @pause
